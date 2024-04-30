9a97a64c9588877c4d1c60fe5cb604a25400605c
Crowdbotics <team@crowdbotics.com>
2024-04-30 19:22:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dolovo-auction-47756",
+  url: "https://dolovo-auction-47756.botics.co",
+  api: "https://dolovo-auction-47756.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
